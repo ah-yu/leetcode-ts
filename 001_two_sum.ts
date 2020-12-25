853551f072679f0d77c11163952019b9c81b1c25
ah-yu <zheyuzhang03@gmail.com>
2020-12-25 18:51:23 +0800

    fix

--- 001_two_sum.ts
+++ 001_two_sum.ts
@@ -1,6 +1,6 @@
 export function twoSum(nums: number[], target: number): number[] {
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i; j < nums.length; j++) {
+    for (let j = i + 1; j < nums.length; j++) {
       const sum = nums[i] + nums[j];
       if (sum == target) {
         return [i, j];
